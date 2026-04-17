00c22a6eab54bf128f1133b7235cca3c3c582ef3
Jonathan Macedo Castro <jonathan.castro18@outlook.com>
2026-04-17 19:22:22 -0300

    feat: adicionando algumas informações iniciais dentro do main e criado o arquivo .gitignore

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-#%%
 import streamlit as st
+import pandas as pd
 
-#%%
-st.text('Olá mundo')
+st.set_page_config(page_title="Finanças", page_icon="💰")
+
+st.markdown("""
+# Boas vindas
+
+## Nosso app financeiro!
+
+Espero que você curta a experiência da nossa solução para organização financeira.
+""")
+
+# Widget de upload de dados
+file_upload = st.file_uploader(label="Faça upload dos dados aqui", type=['csv'])
+if file_upload:
+  df = pd.read_csv(file_upload)
+
+  columns_fmt = {"Valor": st.column_config.NumberColumn("Valor", format="R$ %f")}
+  st.dataframe(df, hide_index=True, column_config=columns_fmt)
